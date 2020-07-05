7a80545fd6ee876412a26179f2c585e581e89a7a
Gabriel Giovani <gabriel.leite4@fatec.sp.gov.br>
2020-07-05 20:54:51 -0300

    add, edit and validate categories

--- app.js
+++ app.js
@@ -1,19 +1,75 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
-//const mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const path = require('path');
+const session = require('express-session');
+const flash = require('connect-flash');
+const moment = require('moment');
+const linkMongo = 'mongodb://localhost/blogapp';
 const port = 3000;
 const ip = '127.0.0.1';
 const app = express();
 const admin = require('./routes/admin');
 
+//Connect MongoDB
+mongoose.Promise = global.Promise;
+
+mongoose.connect(linkMongo).then(() => {
+
+    console.log('Connected in ' + linkMongo);
+
+}).catch((error) => {
+
+    console.log('Error connecting to ' + linkMongo + ': ' + error);
+
+});
+
+//Session
+app.use(session({
+
+    secret: 'blognode',
+    resave: true,
+    saveUninitialized: true
+
+}));
+
+//Flash
+app.use(flash());
+
+//Middleware
+app.use((req, res, next) => {
+
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+
+    next();
+
+});
+
 //Body Parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//Handlebars
-app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
+//Engines
+app.engine('handlebars', handlebars({
+
+    defaultLayout: 'main',
+    helpers:{
+
+        formatDate: (date) => {
+
+            return moment(date).format('DD/MM/YYYY HH:MM');
+
+        }
+
+    }
+
+}));
 app.set('view engine', 'handlebars');
+
+//Public
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Routes
 app.use('/admin', admin);
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+require('../models/Category');
+const Category = mongoose.model('Category');
 
 router.get('/', (req, res) => {
 
-    res.send('Página principal do painel ADM');
+    res.render('../views/admin/index');
 
 });
 
@@ -11,12 +14,169 @@ router.get('/posts', (req, res) => {
 
     res.send('Página de posts');
 
-})
+});
 
 router.get('/cat', (req, res) => {
 
-    res.send('Página de categorias');
+    Category.find().sort([['date', -1]]).lean().then((categories) => {
 
-})
+        res.render('../views/admin/categories', {categories: categories});
+
+    }).catch((error) => {
+
+        req.flash('error_msg', 'Houve um erro ao listar as categorias');
+        res.redirect('/admin');
+
+    })
+
+});
+
+router.get('/cat/add', (req, res) => {
+
+    res.render('../views/admin/addCategories');
+
+});
+
+router.post('/cat/new', (req, res) => {
+
+    let name = req.body.name;
+    let slug = req.body.slug;
+    let errors = [];
+
+    if(!name || typeof name == undefined || name == null){
+
+        errors.push({ text: 'Existem campos vazios!' });
+
+    }
+
+    else if(!slug || typeof slug == undefined || slug == null){
+
+        errors.push({ text: 'Existem campos vazios!' });
+
+    }
+
+    else if(name.length < 2){
+
+        errors.push({ text: 'Nome de categoria muito pequeno!' });
+
+    }
+
+    if(errors.length > 0){
+
+        res.render('../views/admin/addCategories', {errors: errors});
+
+    } else{
+
+        const newCategory = {
+
+            name: name,
+            slug: slug
+    
+        };
+
+        new Category(newCategory).save().then(() => {
+
+            req.flash('success_msg', 'Categoria cadastrada com sucesso!');
+            res.redirect('/admin/cat');
+    
+        }).catch((error) => {
+    
+            req.flash('error_msg', 'Erro ao cadastrar categoria. Tente novamente mais tarde!');
+            res.rendirect('/admin/cat');
+
+            console.log('Register category error: ' + error);
+    
+        });
+
+    }
+
+});
+
+router.get('/cat/edit/:id', (req, res) => {
+
+    Category.findOne({_id:req.params.id}).lean().then((category) => {
+
+        res.render('../views/admin/editCategories', {category: category});
+ 
+    }).catch((error) => {
+
+        req.flash('error_msg', 'Essa categoria não existe!');
+        res.redirect('/admin/cat');
+
+    });
+
+});
+
+router.post('/cat/edit', (req, res) => {
+
+    Category.findOne({_id: req.body.id}).then((category) => {
+
+        let id = req.body.id;
+        let name = req.body.name;
+        let slug = req.body.slug;
+        let errors = [];
+
+        if(!name || typeof name == undefined || name == null){
+
+            errors.push({ text: 'Existem campos vazios!' });
+    
+        }
+    
+        else if(!slug || typeof slug == undefined || slug == null){
+    
+            errors.push({ text: 'Existem campos vazios!' });
+    
+        }
+    
+        else if(name.length < 2){
+    
+            errors.push({ text: 'Nome de categoria muito pequeno!' });
+    
+        }
+    
+        if(errors.length > 0){
+    
+            res.render('../views/admin/editCategories', {
+                errors: errors,
+                category:{
+                    _id: id,
+                    name: name,
+                    slug: slug
+                }
+            });
+    
+        } else{
+
+            category.name = name;
+            category.slug = slug;
+
+            category.save().then(() => {
+
+                req.flash('success_msg', 'Categoria editada com sucesso!');
+                res.redirect('/admin/cat');
+
+            }).catch((error) => {
+
+                req.flash('error_msg', 'Houve um erro ao editar a categoria. Tente mais tarde!');
+                res.redirect('/admin/cat');
+
+            });
+
+        }
+
+    }).catch((error) => {
+
+        req.flash('error_msg', 'Houve um erro ao editar a categoria. Tente mais tarde!');
+        res.redirect('/admin/cat');
+
+    })
+
+});
+
+router.get('/cat/cancel', (req, res) => {
+
+    res.redirect('/admin/cat');
+
+});
 
 module.exports = router;
