5a357b8689244edec0c264a44e86ad840321237d
Gabriel Giovani <gabriel.leite4@fatec.sp.gov.br>
2020-07-10 22:21:26 -0300

    add listing posts and categories

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ const ip = '127.0.0.1';
 const app = express();
 const admin = require('./routes/admin');
 
+require('./models/Posts');
+const Post = mongoose.model('Post');
+require('./models/Category');
+const Category = mongoose.model('Category');
+
 //Connect MongoDB
 mongoose.Promise = global.Promise;
 
@@ -73,6 +78,99 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Routes
 app.use('/admin', admin);
+
+app.get('/', (req, res) => {
+
+    Post.find().lean().populate('category').sort({date: 'desc'}).then((post) => {
+
+        res.render('index', {post: post});
+
+    }).catch((error) => {
+
+        req.flash('error_msg', 'Houve um erro interno. Volte mais tarde!');
+        res.redirect('/404');
+
+    });
+
+});
+
+app.get('/posts/:slug', (req, res) => {
+
+    Post.findOne({slug: req.params.slug}).lean().populate('category').then((post) => {
+
+        if(post){
+
+            res.render('../views/post/index', {post: post});
+
+        } else{
+
+            req.flash('error_msg', 'Esta postagem não existe!');
+            res.redirect('/');
+
+        }
+
+    }).catch((error) => {
+
+        req.flash('error_msg', 'Houve um erro interno!');
+        res.redirect('/');
+
+    });
+
+});
+
+app.get('/categories', (req, res) => {
+
+    Category.find().lean().then((categories) => {
+
+        res.render('../views/category/index', {categories: categories});
+
+    }).catch((error) => {
+
+        req.flash('error_msg', 'Houve um erro ao listar as categorias. Tente mais tarde!');
+        req.redirect('/');
+
+    });
+
+});
+
+app.get('/categories/:slug', (req, res) => {
+
+    Category.findOne({slug: req.params.slug}).lean().then((category) => {
+
+        if(category){
+
+            Post.find({category: category._id}).lean().then((post) => {
+
+                res.render('../views/category/posts', {post: post, category: category});
+
+            }).catch((error => {
+
+                req.flash('error_msg', 'Houve um erro ao carregar as postagens desta categoria. tente mais tarde!');
+                res.redirect('/categories');
+
+            }));
+
+        } else{
+
+            req.flash('error_msg', 'Esta categoria não existe!');
+            res.redirect('/categories');
+
+        }
+
+    }).catch((error) => {
+
+        req.flash('error_msg', 'Houve um erro ao listar as postagens desta categoria. Tente mais tarde!');
+        res.redirect('/categories');
+
+    });
+
+});
+
+app.get('/404', (req, res) => {
+
+    res.send('Error 404!');
+
+})
 
 app.listen(port, ip, () => {
 
